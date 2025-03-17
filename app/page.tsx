b711530209d4e2f590598d6d8be966a58c349d5b
locnv14 <locnv14@gmail.com>
2025-03-17 10:51:17 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,29 @@
 import { Metadata } from "next"
 import HeroForm from "@/components/HeroForm/HeroForm"
-// import { Button } from "components/Button/Button"
-// import { LP_GRID_ITEMS } from "lp-items"
 
 export const metadata: Metadata = {
-  title: "Next.js Enterprise Boilerplate",
+  title: "An Nhiên Labo - Dental Warranty",
   twitter: {
     card: "summary_large_image",
   },
   openGraph: {
-    url: "https://next-enterprise.vercel.app/",
+    url: "https://andentallab.vercel.app/",
     images: [
       {
         width: 1200,
         height: 630,
-        url: "https://raw.githubusercontent.com/Blazity/next-enterprise/main/.github/assets/project-logo.png",
+        url: "/images/logo/logo.png",
+      },
+    ],
+  },
+  icons: {
+    icon: "/images/logo/logo-favicon.png", // Favicon mặc định
+    shortcut: "/images/logo/logo-favicon.png", // Shortcut icon
+    apple: "/images/logo/logo-favicon.png", // Icon cho Apple devices
+    other: [
+      {
+        rel: "icon",
+        url: "/images/logo/logo-favicon.png", // Favicon 32x32
       },
     ],
   },
